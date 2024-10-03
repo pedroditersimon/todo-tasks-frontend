c74b077125d5b06fd5146db45510755ab03aea42
unknown <114523969+pedroditersimon@users.noreply.github.com>
2024-10-03 09:55:08 -0300

    added check icon logic

--- src/components/GoalCard.js
+++ src/components/GoalCard.js
@@ -7,13 +7,23 @@ import ProgressBar from "./ProgressBar";
 import GenericCard from "./GenericCard";
 
 function GoalCard({ goal }) {
+
+    // calculate the goal progress by tasks
+    var goal_progress = 0;
+    if (goal.tasks && goal.tasks.length > 0) {
+        // count completed tasks
+        var completed_tasks_count = goal.tasks.reduce((accumulator, t) => accumulator + (t.isCompleted?1:0), 0);
+        // get progress percent
+        goal_progress = completed_tasks_count/goal.tasks.length;
+    }
+
     return (
         <GenericCard
             title={goal.name}
             primary_icon={star_icon}
-            secondary_icon={check_icon}
+            secondary_icon={goal_progress>=1.0?check_icon:null}
             >
-                <ProgressBar />
+                <ProgressBar value={goal_progress} />
         </GenericCard>
     );
 }
