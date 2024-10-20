92eefc6c989e61e3ce18f37c3582fc8289ab0069
unknown <114523969+pedroditersimon@users.noreply.github.com>
2024-10-20 17:01:45 -0300

    null check

--- src/hooks/useSearchBar.js
+++ src/hooks/useSearchBar.js
@@ -7,7 +7,7 @@ function useSearchBar() {
         if (value === undefined || value === "")
             return true;
 
-        if (text === undefined)
+        if (text === undefined || text == null)
             return false;
         
         return text.toLowerCase().includes(value.toLowerCase());
