b09358ad3482d53b6a2203794622f89021e92f69
unknown <114523969+pedroditersimon@users.noreply.github.com>
2024-10-24 14:56:24 -0300

    added two links in footer and spaced to bottom page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,29 @@
+import github_logo from "../assets/images/github.png";
+import link_icon from "../assets/images/link.png";
+
+import IconButtonText from "./Inputs/IconButtonText";
+import Separator from "./Separator";
+
+import "./Footer.css";
+import Horizontal from "../layouts/Horizontal";
+
 function Footer() {
     return (
-        <div>
-            
+        <div className="footer" >
+
+            <Horizontal>
+                <IconButtonText
+                    icon={github_logo}
+                    href="https://github.com/pedroditersimon/todo-tasks-frontend"
+                    text="GitHub"
+                />
+                <IconButtonText
+                    icon={link_icon}
+                    href="https://pedroditersimon.github.io/"
+                    text="Portfolio"
+                />
+            </Horizontal>
+
         </div>
     );
 }
